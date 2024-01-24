f0a413e9ee456d5c8842f11d455a73777a2eb32e
Saif-Ur-Rehmaan <aptech.saif@gmail.com>
2024-01-24 10:55:24 +0500

    Repos Crweated

--- KarnelTravelAgency.Repository/obj/Debug/net8.0/KarnelTravelAgency.Repository.AssemblyInfo.cs
+++ KarnelTravelAgency.Repository/obj/Debug/net8.0/KarnelTravelAgency.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravelAgency.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396f904abd4115020cc1b9ee11a05e16bc625df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravelAgency.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravelAgency.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
